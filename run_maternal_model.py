3a9f5a1c6fc3ade59e0240a6e850b5bb2374ccca
kforeman <kfor@uw.edu>
2010-10-01 21:38:38 -0700

    missing covariates seem to cause likelihood problems, removed them from stata file for now

--- run_maternal_model.py
+++ run_maternal_model.py
@@ -15,7 +15,7 @@ def load_maternal() :
 
 	# load in the maternal dataset
 	import numpy
-	maternal_raw = stata_to_python.genfromdta('/home/j/Project/Causes of Death/CoDMod/Archive/CODMOD/Maternal Reviewer Responses/Final Run 3/maternal_db_from_paper_for_python.dta', missing_flt=numpy.NaN)
+	maternal_raw = stata_to_python.genfromdta('/home/j/Project/Causes of Death/CoDMod/pymc-space-time-model/maternal_paper_db_no_missing.dta', missing_flt=numpy.nan)
 
 	'''
 	# create dummy variables by age/region
@@ -31,12 +31,12 @@ def load_maternal() :
 	
 	# return the appropriately shaped array
 	'''	x0: ln_LDI, x1: ln_LDI_sq, x2: educ, x3: neonatal_mort, x4: tfr, x5: year, x6: age15, x7: age20, x8: age25, x9: age35, x10: age40, x11: age45 '''
-	return numpy.array(zip(maternal_raw['region'], maternal_raw['iso3'], stdize(maternal_raw['year']), stdize(maternal_raw['age']), maternal_raw['ln_rate'], stdize(maternal_raw['ln_rate_sd']), stdize(numpy.log(maternal_raw['LDI_id'])), stdize(numpy.log(maternal_raw['LDI_id'])**2), stdize(maternal_raw['educ']), stdize(maternal_raw['q_nn_med']), stdize(maternal_raw['tfr']), stdize(maternal_raw['year']), (maternal_raw['age']==15).astype(float), (maternal_raw['age']==20).astype(float), (maternal_raw['age']==25).astype(float), (maternal_raw['age']==35).astype(float), (maternal_raw['age']==40).astype(float), (maternal_raw['age']==45).astype(float),	numpy.zeros(len(maternal_raw)), numpy.zeros(len(maternal_raw)),	numpy.zeros(len(maternal_raw)),	numpy.zeros(len(maternal_raw)),	numpy.zeros(len(maternal_raw)),	numpy.zeros(len(maternal_raw)),	numpy.zeros(len(maternal_raw)),	numpy.zeros(len(maternal_raw)), numpy.zeros(len(maternal_raw)), numpy.zeros(len(maternal_raw)), numpy.zeros(len(maternal_raw)), numpy.zeros(len(maternal_raw))), dtype=[('region','|S35'),('country','|S3'),('year','<f4'),('age','<f4'),('y','<f4'),('se','<f4'),('x0','<f4'),('x1','<f4'),('x2','<f4'),('x3','<f4'),('x4','<f4'),('x5','<f4'),('x6','<f4'),('x7','<f4'),('x8','<f4'),('x9','<f4'),('x10','<f4'),('x11','<f4'),('w0','<f4'),('w1','<f4'),('w2','<f4'),('w3','<f4'),('w4','<f4'),('w5','<f4'),('w6','<f4'),('w7','<f4'),('w8','<f4'),('w9','<f4'),('w10','<f4'),('w11','<f4')]).view(numpy.recarray)
+	return numpy.array(zip(maternal_raw['region'], maternal_raw['iso3'], stdize(maternal_raw['year']), stdize(maternal_raw['age']), maternal_raw['ln_rate'], maternal_raw['ln_rate_sd'], stdize(numpy.log(maternal_raw['LDI_id'])), stdize(numpy.log(maternal_raw['LDI_id'])**2), stdize(maternal_raw['educ']), stdize(maternal_raw['q_nn_med']), stdize(maternal_raw['tfr']), stdize(maternal_raw['year']), (maternal_raw['age']==15).astype(float), (maternal_raw['age']==20).astype(float), (maternal_raw['age']==25).astype(float), (maternal_raw['age']==35).astype(float), (maternal_raw['age']==40).astype(float), (maternal_raw['age']==45).astype(float),	numpy.zeros(len(maternal_raw)), numpy.zeros(len(maternal_raw)),	numpy.zeros(len(maternal_raw)),	numpy.zeros(len(maternal_raw)),	numpy.zeros(len(maternal_raw)),	numpy.zeros(len(maternal_raw)),	numpy.zeros(len(maternal_raw)),	numpy.zeros(len(maternal_raw)), numpy.zeros(len(maternal_raw)), numpy.zeros(len(maternal_raw)), numpy.zeros(len(maternal_raw)), numpy.zeros(len(maternal_raw))), dtype=[('region','|S35'),('country','|S3'),('year','<f8'),('age','<f8'),('y','<f8'),('se','<f8'),('x0','<f8'),('x1','<f8'),('x2','<f8'),('x3','<f8'),('x4','<f8'),('x5','<f8'),('x6','<f8'),('x7','<f8'),('x8','<f8'),('x9','<f8'),('x10','<f8'),('x11','<f8'),('w0','<f8'),('w1','<f8'),('w2','<f8'),('w3','<f8'),('w4','<f8'),('w5','<f8'),('w6','<f8'),('w7','<f8'),('w8','<f8'),('w9','<f8'),('w10','<f8'),('w11','<f8')]).view(numpy.recarray)
 
 # run the model	
 data = load_maternal()
 from model import *
-gp_re_a(data)
+# gp_fit = gp_re_a(data)
 
 
 

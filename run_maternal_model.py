73b34c785575f0e68c1c5bc16cbeaa7f8184d60a
kforeman <kfor@uw.edu>
2010-12-12 20:30:46 -0800

    added gaussian random effects over time by age, removed covariate uncertainty

--- run_maternal_model.py
+++ run_maternal_model.py
@@ -1,8 +1,12 @@
+import numpy
+import stata_to_python
+from numpy.lib import recfunctions as rf
+
+
 def stdize(x) :
-	import numpy
 	return (x-numpy.mean(x))/numpy.std(x)
 
-import stata_to_python
+
 
 def load_maternal() :
 	'''
@@ -14,9 +18,8 @@ def load_maternal() :
 	'''
 
 	# load in the maternal dataset
-	import numpy
-	# maternal_raw = stata_to_python.genfromdta('/home/j/Project/Causes of Death/CoDMod/pymc-space-time-model/maternal_paper_db_no_missing.dta', missing_flt=numpy.nan)
-	maternal_raw = stata_to_python.genfromdta('/home/j/Project/Causes of Death/CoDMod/pymc-space-time-model/sample_dataset.dta', missing_flt=numpy.nan)
+	maternal_raw = stata_to_python.genfromdta('/home/j/Project/Causes of Death/CoDMod/pymc-space-time-model/maternal_paper_db_no_missing.dta', missing_flt=numpy.nan)
+	# maternal_raw = stata_to_python.genfromdta('/home/j/Project/Causes of Death/CoDMod/pymc-space-time-model/sample_dataset.dta', missing_flt=numpy.nan)
 
 	'''
 	# create dummy variables by age/region
@@ -32,16 +35,55 @@ def load_maternal() :
 	
 	# return the appropriately shaped array
 	'''	x0: ln_LDI, x1: ln_LDI_sq, x2: educ, x3: neonatal_mort, x4: tfr, x5: year, x6: age15, x7: age20, x8: age25, x9: age35, x10: age40, x11: age45 '''
-	return numpy.array(zip(maternal_raw['region'], maternal_raw['iso3'], stdize(maternal_raw['year']), stdize(maternal_raw['age']), maternal_raw['ln_rate'], maternal_raw['ln_rate_sd'], stdize(numpy.log(maternal_raw['LDI_id'])), stdize(numpy.log(maternal_raw['LDI_id'])**2), stdize(maternal_raw['educ']), stdize(maternal_raw['q_nn_med']), stdize(maternal_raw['tfr']), stdize(maternal_raw['year']), (maternal_raw['age']==15).astype(float), (maternal_raw['age']==20).astype(float), (maternal_raw['age']==25).astype(float), (maternal_raw['age']==35).astype(float), (maternal_raw['age']==40).astype(float), (maternal_raw['age']==45).astype(float),	numpy.zeros(len(maternal_raw)), numpy.zeros(len(maternal_raw)),	numpy.zeros(len(maternal_raw)),	numpy.zeros(len(maternal_raw)),	numpy.zeros(len(maternal_raw)),	numpy.zeros(len(maternal_raw)),	numpy.zeros(len(maternal_raw)),	numpy.zeros(len(maternal_raw)), numpy.zeros(len(maternal_raw)), numpy.zeros(len(maternal_raw)), numpy.zeros(len(maternal_raw)), numpy.zeros(len(maternal_raw))), dtype=[('region','|S35'),('country','|S3'),('year','<f8'),('age','<f8'),('y','<f8'),('se','<f8'),('x0','<f8'),('x1','<f8'),('x2','<f8'),('x3','<f8'),('x4','<f8'),('x5','<f8'),('x6','<f8'),('x7','<f8'),('x8','<f8'),('x9','<f8'),('x10','<f8'),('x11','<f8'),('w0','<f8'),('w1','<f8'),('w2','<f8'),('w3','<f8'),('w4','<f8'),('w5','<f8'),('w6','<f8'),('w7','<f8'),('w8','<f8'),('w9','<f8'),('w10','<f8'),('w11','<f8')]).view(numpy.recarray)
+	data = zip(maternal_raw['region'],
+		maternal_raw['iso3'],
+		maternal_raw['year'],
+		maternal_raw['age'],
+		maternal_raw['ln_rate'],
+		maternal_raw['ln_rate_sd'],
+		stdize(numpy.log(maternal_raw['LDI_id'])),
+		stdize(numpy.log(maternal_raw['LDI_id'])**2),
+		stdize(maternal_raw['educ']),
+		stdize(maternal_raw['q_nn_med']),
+		stdize(maternal_raw['tfr']),
+		stdize(maternal_raw['year']),
+		(maternal_raw['age']==15).astype(float),
+		(maternal_raw['age']==20).astype(float),
+		(maternal_raw['age']==25).astype(float),
+		(maternal_raw['age']==35).astype(float),
+		(maternal_raw['age']==40).astype(float),
+		(maternal_raw['age']==45).astype(float))
+	''',	numpy.zeros(len(maternal_raw)), numpy.zeros(len(maternal_raw)),	numpy.zeros(len(maternal_raw)),	numpy.zeros(len(maternal_raw)),	numpy.zeros(len(maternal_raw)),	numpy.zeros(len(maternal_raw)),	numpy.zeros(len(maternal_raw)),	numpy.zeros(len(maternal_raw)), numpy.zeros(len(maternal_raw)), numpy.zeros(len(maternal_raw)), numpy.zeros(len(maternal_raw)), numpy.zeros(len(maternal_raw))'''
+
+	# filter data for fast debugging and testing
+	# data = [d for d in data if d[0].startswith('Asia')]
+	print 'data rows: ', len(data)
+	assert len(data) > 0
+	
+	'''return numpy.array(data, dtype=[('region','|S35'),('country','|S3'),('year','<f8'),('age','<f8'),('y','<f8'),('se','<f8'),('x0','<f8'),('x1','<f8'),('x2','<f8'),('x3','<f8'),('x4','<f8'),('x5','<f8'),('x6','<f8'),('x7','<f8'),('x8','<f8'),('x9','<f8'),('x10','<f8'),('x11','<f8'),('w0','<f8'),('w1','<f8'),('w2','<f8'),('w3','<f8'),('w4','<f8'),('w5','<f8'),('w6','<f8'),('w7','<f8'),('w8','<f8'),('w9','<f8'),('w10','<f8'),('w11','<f8')]).view(numpy.recarray)'''
+	return numpy.array(data, dtype=[('region','|S35'),('country','|S3'),('year','<f8'),('age','<f8'),('y','<f8'),('se','<f8'),('x0','<f8'),('x1','<f8'),('x2','<f8'),('x3','<f8'),('x4','<f8'),('x5','<f8'),('x6','<f8'),('x7','<f8'),('x8','<f8'),('x9','<f8'),('x10','<f8'),('x11','<f8')]).view(numpy.recarray)
+	
+	
+
 
 # run the model	
 data = load_maternal()
+from pylab import rec2csv
+rec2csv(data, 'maternal_asia.csv')
 from model import *
+print('Data loaded')
 mod_mc = gp_re_a(data)
-iter = 20000
-mod_mc.sample(iter, iter/2, iter/2000, verbose=1)
+print('Initial optimization complete')
+iter = 5000
+mod_mc.sample(iter, burn=1000, thin=10, verbose=1)
+print('MCMC sampling complete')
 
 
+
+# save the results
+predicted_y = mod_mc.param_predicted.stats()['mean']
+results = rf.append_fields(data, 'prediction', predicted_y)
+rec2csv(results, '/home/j/Project/Causes of Death/CoDMod/pymc-space-time-model/maternal_results_all_data.csv')
 
 
 

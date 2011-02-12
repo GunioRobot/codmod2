537b332671066b39a3589dbec01c9a37ae0518b2
kforeman <kfor@uw.edu>
2011-02-11 19:14:14 -0800

    added lots of data loading functions

--- codmod_class.py
+++ codmod_class.py
@@ -1,6 +1,6 @@
 '''
 Author:	    Kyle Foreman
-Date:	    09 February 2011
+Date:	    10 February 2011
 Purpose:    Fit cause of death models over space, time, and age
 '''
 
@@ -9,6 +9,8 @@ import numpy as np
 import MySQLdb
 from scipy import interpolate
 import matplotlib as plot
+from matplotlib.mlab import rec_join
+import numpy.lib.recfunctions as recfunctions
 
 class codmod:
     '''
@@ -17,7 +19,7 @@ class codmod:
         set_window:         the range of ages/years to predict over
         set_pi_samples:     at what ages/years should pi (the random effect component) be sampled?
         set_covariates:     set which covariates to use in the model
-        list_causes:        lists the codmod causes available for easy reference
+        list_cause_names:   lists the codmod causes available for easy reference
         list_covariates:    lists the covariates available for easy reference
         load:               query the mysql database for the appropriate data
         initialize_model:   create the model object and find starting values via MAP
@@ -28,17 +30,37 @@ class codmod:
 
     def __init__(self, cause, sex):
         ''' Specify cause (string like 'Aa02', 'Ab10', 'B142', 'C241', etc) and sex ('male' or 'female') '''
+        self.connect()
         self.cause = cause
-        if (sex=='male'):
-            self.sex = 1
-        elif (sex=='female'):
-            self.sex = 2
+        self.data_rows = 0
+        self.sex = sex
+        if (self.sex=='male'):
+            self.sex_num = 1
+        elif (self.sex=='female'):
+            self.sex_num = 2
         else:
             raise ValueError("Specify sex as either 'male' or 'female'")
-        self.connect()
+        print 'Sex:', self.sex
+        print 'Cause:', self.cause
+        self.list_cause_names(cause=self.cause)
         self.set_covariates()
         self.set_window()
         self.set_pi_samples()
+
+
+    def model_setup(self):
+        print 'Cause:', self.cause
+        print 'Sex:', self.sex
+        print 'Age Range:', self.age_range
+        print 'Age Samples:', self.age_samples
+        print 'Year Range:', self.year_range
+        print 'Year Samples:', self.year_samples
+        if self.data_rows == 0:
+            print 'Data Rows: Not Loaded'
+        else:
+            print 'Data Rows:', self.data_rows
+        print 'Covariates:', self.covariate_list
+        print 'Age Dummies:', self.age_dummies
 
 
     def set_window(self, age_range=[0,80], year_range=[1980,2010]):
@@ -80,34 +102,51 @@ class codmod:
         return self.cursor.fetchall()
 
 
-    def list_causes(self):
+    def list_cause_names(self, cause=''):
         ''' Return a list mapping cause codes with names '''
-        self.cursor.execute("SELECT cod_cause,cod_cause_name FROM cod_causes WHERE substr(cod_cause,length(cod_cause),1)!='x';")
-        return self.cursor.fetchall()
+        if cause == '':
+            self.cursor.execute('SELECT cod_cause,cod_cause_name FROM cod_causes WHERE substr(cod_cause,length(cod_cause),1)!="x";')
+            return self.cursor.fetchall()
+        else:
+            self.cursor.execute('SELECT cod_cause_name FROM cod_causes WHERE cod_cause="' + cause + '";')
+            self.cause_name = self.cursor.fetchall()[0][0]
+            print 'Cause Name:', self.cause_name
 
 
-    def set_covariates(self, covariate_list=['education_years_pc'], age_dummies=True):
+    def set_covariates(self, covariate_list=['year','education_yrs_pc'], age_dummies=True, age_ref=30, normalize=True):
         '''
         By default, the model will just use education as a covariate, plus age dummies.
         Calling this method with a list of covariates will set the model to use those instead.
-        In addition, some simple transformations (ln() = natural log) are allowed.
+        In addition, some simple transformations (ln(covariate) = natural log, (covariate)^2 = squared) are allowed.
         
         For example, to use ln(LDI) and education as covariates, use this syntax:
             codmod.set_covariates(['education_yrs_pc','ln(LDI_pc)'])
         '''
+        if type(covariate_list) == str:
+            covariate_list = [covariate_list]
         self.covariate_list = covariate_list
         self.covariates_untransformed = []
         self.covariate_transformations = []
         for c in covariate_list:
-            if c[0:3] == 'ln(':
+            if c[0:3] == 'ln(' and c[-2:] != '^2':
                 self.covariates_untransformed.append(c[3:len(c)-1])
                 self.covariate_transformations.append('ln')
+            elif c[0:3] == 'ln(' and c[-2:] == '^2':
+                self.covariates_untransformed.append(c[3:len(c)-3])
+                self.covariate_transformations.append('ln+sq')
+            elif c[-2:] == '^2':
+                self.covariates_untransformed.append(c[:len(c)-3])
+                self.covariate_transformations.append('sq')
             else:
                 self.covariates_untransformed.append(c)
-                self.covariate_transformations.append('')
+                self.covariate_transformations.append('none')
         self.age_dummies = age_dummies
-        print 'Covariates:', covariate_list
-        print 'Age Dummies:', age_dummies
+        self.age_ref = age_ref
+        self.normalize = normalize
+        print 'Covariates:', self.covariate_list
+        print 'Age Dummies:', self.age_dummies
+        print 'Reference Age:', self.age_ref
+        print 'Normalize Covariates:', self.normalize
 
 
     def load(self):
@@ -115,11 +154,64 @@ class codmod:
         Loads codmod data from the MySQL server.
         The resulting query will get all the data for a specified cause and sex, plus any covariates specified
         '''
-        sql = 'SELECT '
+        # load in selected covariates
+        covs = ''
+        for i in list(set(self.covariates_untransformed)):
+            if i != 'year':
+                covs = covs + i + ','
+        cov_sql = 'SELECT iso3,region,age,year,sex,' + covs[0:-1] + ' FROM all_covariates WHERE sex=' + str(self.sex_num) + ' AND age BETWEEN ' + str(self.age_range[0]) + ' AND ' + str(self.age_range[1]) + ' AND year BETWEEN ' + str(self.year_range[0]) + ' AND ' + str(self.year_range[1])
+        print 'Loading covariates...'
+        covariate_data = mysql_to_recarray(self.cursor, cov_sql)        
+
+        # make covariate matrix (including transformations and normalization)
+        covariate_vectors = [covariate_data.iso3, covariate_data.region, covariate_data.year, covariate_data.age, covariate_data.sex, np.ones(covariate_data.shape[0])]
+        covariate_names = ['iso3', 'region', 'year', 'age', 'sex', 'x0']
+        self.covariate_dict = {'x0': 'constant'}
+        for i in range(len(self.covariate_list)):
+            j = covariate_data[self.covariates_untransformed[i]]
+            if self.covariate_transformations[i] == 'ln':
+                j = np.log(j)
+            elif self.covariate_transformations[i] == 'ln+sq':
+                j = (np.log(j))**2
+            elif self.covariate_transformations[i] == 'sq':
+                j = j**2
+            if self.normalize == True:
+                j = ((j-np.mean(j))/np.std(j))
+            covariate_vectors.append(j)
+            covariate_names.append('x' + str(i+1))
+            self.covariate_dict['x' + str(i+1)] = self.covariate_list[i]
+
+        # create age dummies if specified
+        if self.age_dummies == True:
+            pre_ref = 1
+            for i,a in enumerate(np.unique(covariate_data.age)):
+                if a == self.age_ref:
+                    pre_ref = 0
+                elif pre_ref == 1:
+                    covariate_vectors.append(np.array(covariate_data.age==a).astype(np.float))
+                    covariate_names.append('x' + str(len(self.covariate_list)+i+1))
+                    self.covariate_dict['x' + str(len(self.covariate_list)+i+1)] = 'Age ' + str(a)
+                else:
+                    covariate_vectors.append(np.array(covariate_data.age==a).astype(np.float))
+                    covariate_names.append('x' + str(len(self.covariate_list)+i))
+                    self.covariate_dict['x' + str(len(self.covariate_list)+i)] = 'Age ' + str(a)
+        self.covariate_matrix = np.core.records.fromarrays(covariate_vectors, names=covariate_names)
+
+        # load in death observations
+        deaths_sql = 'SELECT cf,envelope,pop,iso3,year,sex,age,sample_size FROM deaths_by_cause_tab WHERE cod_cause="' + self.cause + '" AND sex=' + str(self.sex_num) + ' AND age BETWEEN ' + str(self.age_range[0]) + ' AND ' + str(self.age_range[1]) + ' AND year BETWEEN ' + str(self.year_range[0]) + ' AND ' + str(self.year_range[1])
+        print 'Loading death data...'
+        self.death_obs = mysql_to_recarray(self.cursor, deaths_sql)
+
+        # prep all the in-sample data
+        self.training_data = rec_join(['iso3','year','age','sex'], self.death_obs, self.covariate_matrix)
+        self.data_rows = self.training_data.shape[0]
+        print 'Data Rows:', self.data_rows
 
 
-    def plot_data(self):
-        return something
+    def plot_data(self, iso3=''):
+        if iso3:
+            return something
+
 
     def initialize_model(self, find_start_vals=True):
         '''
@@ -246,13 +338,11 @@ class codmod:
         @mc.observed
         def data_likelihood(value=self.data.y, i=obs_index, mu=param_pred, tau=tau_pred):
             return mc.normal_like(value[i], mu[i], tau[i])
-        
+
         # create a pickle backend to store the model
-        '''
         import time as tm
-        dbname = '/home/j/Project/Causes of Death/CoDMod/tmp files/codmod_' + str(np.int(tm.time()))
+        dbname = '/home/j/Project/Causes of Death/CoDMod/tmp files/codmod_' + self.cause + '_' + tm.strftime('%b%d_%I%M%p')
         db = mc.database.pickle.Database(dbname=dbname, dbmode='w')
-        '''
 
         # MCMC step methods
         self.mod_mc = mc.MCMC(vars(), db=db)
@@ -280,4 +370,23 @@ class codmod:
         self.mod_mc.sample(iter=iter, burn=burn, thin=thin, verbose=verbose)
 
 
+def mysql_to_recarray(cursor, query):
+    ''' Makes a MySQL query and returns the results as a record array '''
+    cursor.execute(query)
+    data = cursor.fetchall()
+    data = [data[i] for i in range(len(data))]
+    cols = np.array([cursor.description[i][0:2] for i in range(len(cursor.description))])
+    for i in range(len(cols)) :
+        t = cols[i][1]
+        if t == '1' or t == '2' or t == '9' or t == '13':
+            cols[i][1] = '<i4'
+        elif t == '4' or t == '5' or t =='8' or t == '3':
+            cols[i][1] = '<f8'
+        else:
+            str_l = 1
+            for j in range(len(data)) :
+                str_l = max((str_l, len(data[j][i])))
+            cols[i][1] = '<S' + str(str_l)
+    cols = [(cols[i][0], cols[i][1]) for i in range(len(cols))]
+    return np.array(data, dtype=cols).view(np.recarray)
 

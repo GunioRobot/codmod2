934c7bbfb1bdf5af018f186d4759333d77385589
kforeman <kfor@uw.edu>
2011-02-27 18:39:20 -0800

    folder cleanup

--- qsub_model.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#$ -M kfor@uw.edu
-#$ -m beas
-cd "/home/j/Project/Causes of Death/CoDMod/codmod2/"
-/usr/local/epd_py25-4.3.0/bin/ipython "/home/j/Project/Causes of Death/CoDMod/codmod2/run_model.py"

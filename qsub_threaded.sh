f7eef5c8a32c3c86c694d5ba411652b4a24f3e58
kforeman <kfor@uw.edu>
2011-03-03 10:50:40 -0800

    threading: herein lie lots threaded attempts, none of which actually improved performance. after this commit they will be struck from the record, never to see the light of day again. I am so vastly disappointed in them that I am loathe to even see their filenames in my browser.

--- qsub_threaded.sh
+++ qsub_threaded.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #$ -S /bin/sh
 cd "/home/j/Project/Causes of Death/CoDMod/codmod2/"
-/usr/local/epd_py25-4.3.0/bin/ipython "/home/j/Project/Causes of Death/CoDMod/codmod2/codmod_no_class.py"
+/usr/local/epd_py25-4.3.0/bin/ipython "/home/j/Project/Causes of Death/CoDMod/codmod2/codmod_no_class_6.py"

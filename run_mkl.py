d13d502f199083c62e74caac057133e56f28dae2
kforeman <kfor@uw.edu>
2011-03-03 17:28:26 -0800

    version that uses NormApprox to find marginal uncertainty - but NormApprox does a really bad job for some reason

--- run_mkl.py
+++ run_mkl.py
@@ -1,31 +1,28 @@
 # load the codmod program
 import os
 os.chdir('/home/j/Project/Causes of Death/CoDMod/codmod2/')
-import codmod_normapprox as cm
+import codmod as cm
 reload(cm)
 
 # setup multithreading
-#import mkl
-#mkl.set_num_threads(1)
+import mkl
+mkl.set_num_threads(8)
 
 # setup the model
-m = cm.codmod('Ab10','female','maternal_test', just_testing=True)
+m = cm.codmod('Ab10','female','maternal_test')
 m.set_covariates(covariate_list=['year','education_yrs_pc','ln(TFR)','neonatal_deaths_per1000','ln(LDI_pc)','HIV_prevalence_pct'], age_dummies=True, age_ref=30, normalize=True)
 m.set_window(age_range=[15,45], year_range=[1980,2010])
 m.set_pi_samples(age_samples=[15,25,35,45], year_samples=[1980,1990,2000,2010])
 
 # load in the data
 m.load(use_cache=True)
-#m.training_split(holdout_unit='none')
+m.training_split(holdout_unit='none')
 
 # build the model and use MAP to find starting values
-m.initialize_model()
-
-m.MAP(iters=1)
-#m.NormApprox(samples=10)
+m.initialize_model(find_start_vals=True)
 
 # use MCMC to find posterior
-#m.sample(iter=1000, burn=0, thin=1)
+m.sample(iter=1000, burn=0, thin=1)
 
 # make predictions
 #m.predict_test(save_csv=False)
@@ -34,5 +31,5 @@ m.MAP(iters=1)
 #m.measure_fit()
 
 # make MCMC diagnostic plots
-#m.mcmc_diagnostics()
+m.mcmc_diagnostics()
 
